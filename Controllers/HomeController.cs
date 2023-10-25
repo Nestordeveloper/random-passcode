5175c85ee5c8d4ba37c59481201fbfd550d9e462
Nestor Larenas <sne.larenasortega@hotmail.com>
2023-10-24 23:26:54 -0300

    Random Passcode Actividad COmpletada

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,59 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using random_passcode.Models;
+
+namespace random_passcode.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    [HttpGet]
+    [Route("")]
+    public IActionResult Index()
+    {
+        return View("Index");
+    }
+
+    [HttpPost]
+    public IActionResult Generate(string generateCode)
+    {
+        int passCode = HttpContext.Session.GetInt32("PassCode") ?? 0;
+
+        if (generateCode == "start")
+        {
+            string randomPassword = GenerateRandomPassword(14);
+            passCode += 1;
+
+            HttpContext.Session.SetString("RandomPassword", randomPassword);
+            HttpContext.Session.SetInt32("PassCode", passCode);
+        }
+
+        return RedirectToAction("Index");
+    }
+
+    private string GenerateRandomPassword(int length)
+    {
+        const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+        var random = new Random();
+        var password = new System.Text.StringBuilder(length);
+
+        for (int i = 0; i < length; i++)
+        {
+            password.Append(chars[random.Next(chars.Length)]);
+        }
+
+        return password.ToString();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
---
+++ Models/PasswordViewModel.cs
@@ -0,0 +1,6 @@
+#pragma warning disable CS8618
+using System.ComponentModel.DataAnnotations;
+public class Password
+{
+    public string GeneratedPassword { get; set; }
+}
